cee42f11ac9ac996f301ffc92ab988e4871b65f9
Chong Chen <chongchen1997@gmail.com>
2020-11-30 12:33:40 +0800

    Implement the error toasts of trade sheet

--- Stock Search/DetailScreenViews/TradeSheetView.swift
+++ Stock Search/DetailScreenViews/TradeSheetView.swift
@@ -14,15 +14,55 @@ struct TradeSheetView: View {
     @State var showBoughtView: Bool = false
     @State var showSoldView: Bool = false
     
+    @State var input: String = ""
+    
+    @State var showErrorNotEnoughShares: Bool = false //triggered by the Sell button
+    @State var showErrorNotEnoughMoney: Bool = false //triggered by the Buy button
+    @State var showErrorSellNonpositiveShares: Bool = false //triggered by the Sell button
+    @State var showErrorBuyNonpositiveShares: Bool = false //triggered by the Buy button
+    @State var showErrorInvalidInput: Bool = false //triggered by BOTH two buttons
+    
     var body: some View {
         NavigationView {
             if (!showBoughtView && !showSoldView) {
                 VStack {
                     Text("Trade \(stock.name) shares")
+                        .fontWeight(.bold)
+                    Spacer()
+                    VStack {
+                        HStack {
+                            TextField("0", text: $input)
+                                .keyboardType(.numberPad)
+                                .font(.system(size: 100))
+                            Spacer()
+                            Text("Share")
+                                .font(.title)
+                        }
+                        HStack {
+                            Spacer()
+                            Text("x $\(getLatestPriceInfo(ticker: stock.ticker).lastPrice, specifier: "%.2f")/Share = $\(getCalcWorth(), specifier: "%.2f")")
+                        }
+                    }
+                    Spacer()
                     Text("$\(getAvailableWorth(), specifier: "%.2f") available to buy \(stock.ticker)")
+                        .font(.footnote)
+                        .foregroundColor(Color.gray)
+                        .padding(.vertical)
                     HStack {
+                        // Buy button
                         Button(action: {
-                            self.showBoughtView.toggle()
+                            if isValidInput() {
+                                if getAvailableWorth() < getCalcWorth() {
+                                    self.showErrorNotEnoughMoney = true
+                                } else if getNumInput() <= 0 {
+                                    self.showErrorBuyNonpositiveShares = true
+                                } else {
+                                    // buy action
+                                    self.showBoughtView.toggle()
+                                }
+                            } else {
+                                self.showErrorInvalidInput = true
+                            }
                         }) {
                             Text("Buy")
                                 .font(.title3)
@@ -33,8 +73,20 @@ struct TradeSheetView: View {
                                 .cornerRadius(40)
                         }
                         
+                        // Sell button
                         Button(action: withAnimation {{
-                            self.showSoldView.toggle()
+                            if isValidInput() {
+                                if stock.sharesBought < getNumInput() {
+                                    self.showErrorNotEnoughShares = true
+                                } else if getNumInput() <= 0 {
+                                    self.showErrorSellNonpositiveShares = true
+                                } else {
+                                    // sell action
+                                    self.showSoldView.toggle()
+                                }
+                            } else {
+                                self.showErrorInvalidInput = true
+                            }
                         }}) {
                             Text("Sell")
                                 .font(.title3)
@@ -46,6 +98,7 @@ struct TradeSheetView: View {
                         }
                     }
                 }
+                .padding(.horizontal)
                 .toolbar {
                     ToolbarItem(placement: .navigationBarLeading) {
                         Button (action: withAnimation {{
@@ -55,6 +108,21 @@ struct TradeSheetView: View {
                         }
                     }
                 }//toobar
+                .toast(isPresented: self.$showErrorNotEnoughShares) {
+                    Text("Not enough shares to sell")
+                }
+                .toast(isPresented: self.$showErrorNotEnoughMoney) {
+                    Text("Not enough money to buy")
+                }
+                .toast(isPresented: self.$showErrorSellNonpositiveShares) {
+                    Text("Cannot sell less than 0 share")
+                }
+                .toast(isPresented: self.$showErrorBuyNonpositiveShares) {
+                    Text("Cannot buy less than 0 share")
+                }
+                .toast(isPresented: self.$showErrorInvalidInput) {
+                    Text("Please enter a valid amount")
+                }
             } else {
                 ZStack {
                     Color.green
@@ -91,6 +159,26 @@ struct TradeSheetView: View {
             }
         }//NavigationView
     }//body
+    
+    func getNumInput() -> Double {
+        if let num = Double(input) {
+            return num
+        } else {
+            return 0.0
+        }
+    }
+    
+    func isValidInput() -> Bool {
+        if Double(input) != nil {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func getCalcWorth() -> Double {
+        return getNumInput() * getLatestPriceInfo(ticker: stock.ticker).lastPrice
+    }
 }//view
 
 struct TradeSheetView_Previews: PreviewProvider {
